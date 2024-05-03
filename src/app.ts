4f75aaed2887c0282a301c05c77497f04ae82761
belij2111 <belijvalerij2111@gmail.com>
2024-05-04 00:56:52 +0300

    feat: Add a mark about the backend version

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 import cors from "cors"
 import express from "express"
+import {Request, Response} from 'express';
 
 export const app = express()
 app.use(express.json())
 app.use(cors())
+
+app.get("/", (req: Request, res: Response) => {
+    res
+        .status(200)
+        .json({version: '1.0'})
+})
