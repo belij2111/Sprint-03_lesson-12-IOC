b957b08724c17381daa1fafd0861e59be9e55ad3
belij2111 <belijvalerij2111@gmail.com>
2024-05-08 22:26:34 +0300

    feat: Add validation of input data for blogs

--- src/routers/blogs-router.ts
+++ src/routers/blogs-router.ts
@@ -6,11 +6,12 @@ import {
 } from "../controllers/blogs-controller"
 import {authMiddleware} from "../middlewares/auth-middleware";
 import {inputValidationMiddleware} from "../middlewares/input-validation-middlware";
+import {blogsInputValidationMiddleware} from "../middlewares/blogs-input-validation-middleware";
 
 export const blogsRouter = Router()
 
-blogsRouter.post('/', authMiddleware, inputValidationMiddleware, createBlogController)
+blogsRouter.post('/', authMiddleware, blogsInputValidationMiddleware, inputValidationMiddleware, createBlogController)
 blogsRouter.get('/', getBlogsController)
 blogsRouter.get('/:id', getBlogByIdController)
-blogsRouter.put('/:id', authMiddleware, inputValidationMiddleware, updateBlogByIdController)
+blogsRouter.put('/:id', authMiddleware, blogsInputValidationMiddleware, inputValidationMiddleware, updateBlogByIdController)
 blogsRouter.delete('/:id', authMiddleware, inputValidationMiddleware, deleteBlogByIdController)
