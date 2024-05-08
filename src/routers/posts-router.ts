b114ea7ab0f6eca56b52b8fd627a46b375209b0c
belij2111 <belijvalerij2111@gmail.com>
2024-05-08 22:31:11 +0300

    feat: Add validation of input data for posts

--- src/routers/posts-router.ts
+++ src/routers/posts-router.ts
@@ -7,11 +7,12 @@ import {
 } from "../controllers/posts-controller";
 import {authMiddleware} from "../middlewares/auth-middleware";
 import {inputValidationMiddleware} from "../middlewares/input-validation-middlware";
+import {postsInputValidationMiddleware} from "../middlewares/posts-input-validation-middleware";
 
 export const postsRouter = Router()
 
-postsRouter.post('/', authMiddleware, inputValidationMiddleware, createPostController)
+postsRouter.post('/', authMiddleware, postsInputValidationMiddleware, inputValidationMiddleware, createPostController)
 postsRouter.get('/', getPostController)
 postsRouter.get('/:id', getPostByIdController)
-postsRouter.put('/:id', authMiddleware, inputValidationMiddleware, updatePostController)
+postsRouter.put('/:id', authMiddleware, postsInputValidationMiddleware, inputValidationMiddleware, updatePostController)
 postsRouter.delete('/:id', authMiddleware, inputValidationMiddleware, deletePostByIdController)
