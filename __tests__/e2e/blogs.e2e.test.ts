8baea36b1ab1fd1a0de6caf90e5f3c3bd0d42d09
belij2111 <belijvalerij2111@gmail.com>
2024-06-05 23:58:22 +0300

    refactor(test): Move part of the test logic to tests-managers and tests-dtos

---
+++ __tests__/e2e/blogs.e2e.test.ts
@@ -0,0 +1,41 @@
+import {blogCollection, connectToDb} from "../../src/db/mongo-db";
+import {SETTINGS} from "../../src/settings";
+import {blogDto} from "../tests-dtos/blog-dto";
+import {blogsTestManager} from "./tests-managers/blogs-test-Manager";
+
+describe('/blogs', () => {
+    beforeAll(async () => {
+        await connectToDb(SETTINGS.MONGO_URL)
+        await blogCollection.drop()
+    })
+
+    it(`should create new blog : STATUS 201`, async () => {
+        const validBlog = blogDto.createValidBlogDto()
+        const authorizationHeader = await blogsTestManager.createAuthorizationHeader('Basic', SETTINGS.ADMIN_AUTH)
+        const result = await blogsTestManager.createBlog(validBlog, authorizationHeader)
+        // console.log(result.body)
+        // console.log(result.status)
+        expect(result.status).toBe(201)
+        expect(result.body.name).toBe(validBlog.name)
+        expect(result.body.description).toBe(validBlog.description)
+        expect(result.body.websiteUrl).toBe(validBlog.websiteUrl)
+    })
+
+    it(`shouldn't create new blog with incorrect input data : STATUS 400`, async () => {
+        const invalidBlog = blogDto.createInvalidBlogDto()
+        const authorizationHeader = await blogsTestManager.createAuthorizationHeader('Basic', SETTINGS.ADMIN_AUTH)
+        const result = await blogsTestManager.createBlog(invalidBlog, authorizationHeader)
+        // console.log(result.body)
+        // console.log(result.status)
+        expect(result.status).toBe(400)
+    })
+
+    it(`shouldn't create new blog if the request is unauthorized : STATUS 401`, async () => {
+        const validBlog = blogDto.createValidBlogDto()
+        const invalidAuthorizationHeader = await blogsTestManager.createAuthorizationHeader('Basic', '');
+        const result = await blogsTestManager.createBlog(validBlog, invalidAuthorizationHeader)
+        // console.log(result.body)
+        // console.log(result.status)
+        expect(result.status).toBe(401)
+    })
+})
