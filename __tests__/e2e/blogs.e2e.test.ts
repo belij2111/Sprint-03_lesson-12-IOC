d6e5a5377f45cb6c6dfed8d2535d3f8dd1a5b352
belij2111 <belijvalerij2111@gmail.com>
2024-06-07 17:13:37 +0300

    fix(test): Fix test assertion for creating a new blog

--- __tests__/e2e/blogs.e2e.test.ts
+++ __tests__/e2e/blogs.e2e.test.ts
@@ -3,6 +3,8 @@ import {SETTINGS} from "../../src/settings";
 import {blogDto} from "../tests-dtos/blog-dto";
 import {blogsTestManager} from "./tests-managers/blogs-test-Manager";
 import {startMongoServer, stopMongoServer} from "../mongo-memory-setup";
+import {req} from "../test-helpers";
+import {Response} from "supertest";
 
 describe('/blogs', () => {
     beforeAll(async () => {
@@ -19,33 +21,38 @@ describe('/blogs', () => {
     it(`should create new blog : STATUS 201`, async () => {
         const validBlog = blogDto.createValidBlogDto()
         const authorizationHeader = await blogsTestManager.createAuthorizationHeader('Basic', SETTINGS.ADMIN_AUTH)
-        const result = await blogsTestManager.createBlog(validBlog, authorizationHeader)
-        console.log(result.body)
-        console.log(result.status)
-
-        expect(result.status).toBe(201)
-        expect(result.body.name).toBe(validBlog.name)
-        expect(result.body.description).toBe(validBlog.description)
-        expect(result.body.websiteUrl).toBe(validBlog.websiteUrl)
+        const result: Response = await req
+            .post(SETTINGS.PATH.BLOGS)
+            .set(authorizationHeader)
+            .send(validBlog)
+            .expect(201)
+        expect(result.body).toEqual({
+            id: expect.any(String),
+            name: validBlog.name,
+            description: validBlog.description,
+            websiteUrl: validBlog.websiteUrl,
+            createdAt: expect.any(String),
+            isMembership: expect.any(Boolean)
+        })
     })
 
     it(`shouldn't create new blog with incorrect input data : STATUS 400`, async () => {
         const invalidBlog = blogDto.createInvalidBlogDto()
         const authorizationHeader = await blogsTestManager.createAuthorizationHeader('Basic', SETTINGS.ADMIN_AUTH)
-        const result = await blogsTestManager.createBlog(invalidBlog, authorizationHeader)
-        console.log(result.body)
-        console.log(result.status)
-
-        expect(result.status).toBe(400)
+        await req
+            .post(SETTINGS.PATH.BLOGS)
+            .set(authorizationHeader)
+            .send(invalidBlog)
+            .expect(400)
     })
 
     it(`shouldn't create new blog if the request is unauthorized : STATUS 401`, async () => {
         const validBlog = blogDto.createValidBlogDto()
-        const invalidAuthorizationHeader = await blogsTestManager.createAuthorizationHeader('Basic', '');
-        const result = await blogsTestManager.createBlog(validBlog, invalidAuthorizationHeader)
-        console.log(result.body)
-        console.log(result.status)
-
-        expect(result.status).toBe(401)
+        const invalidAuthorizationHeader = await blogsTestManager.createAuthorizationHeader('Basic', 'invalid');
+        await req
+            .post(SETTINGS.PATH.BLOGS)
+            .set(invalidAuthorizationHeader)
+            .send(validBlog)
+            .expect(401)
     })
 })
