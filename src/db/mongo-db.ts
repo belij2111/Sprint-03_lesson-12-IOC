a2bec0911544d5349d3c346ea701977e0db33b64
belij2111 <belijvalerij2111@gmail.com>
2024-06-12 22:41:04 +0300

    feat: Add collection for users in db

--- src/db/mongo-db.ts
+++ src/db/mongo-db.ts
@@ -2,12 +2,14 @@ import {Collection, Db, MongoClient} from "mongodb"
 import {SETTINGS} from "../settings"
 import {BlogDBType} from "./blog-db-type";
 import {PostDbType} from "./post-db-type";
+import {UserDbType} from "./user-db-type";
 
 let client = {} as MongoClient
 export let db = {} as Db
 
 export let blogCollection: Collection<BlogDBType> = {} as Collection<BlogDBType>
 export let postCollection: Collection<PostDbType> = {} as Collection<PostDbType>
+export let userCollection: Collection<UserDbType> = {} as Collection<UserDbType>
 
 export const connectToDb = async (DB_URL: string) => {
     try {
@@ -15,6 +17,7 @@ export const connectToDb = async (DB_URL: string) => {
         db = client.db(SETTINGS.DB_NAME)
         blogCollection = db.collection<BlogDBType>(SETTINGS.BLOG_COLLECTION_NAME)
         postCollection = db.collection<PostDbType>(SETTINGS.POST_COLLECTION_NAME)
+        userCollection = db.collection<UserDbType>(SETTINGS.USER_COLLECTION_NAME)
         await client.connect()
         console.log('Connected to db')
         return true
