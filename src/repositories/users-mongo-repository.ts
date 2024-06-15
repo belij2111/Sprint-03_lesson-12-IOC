2359e34d93a7c5bccc1a10d40a03189861ad7c3f
belij2111 <belijvalerij2111@gmail.com>
2024-06-15 18:44:01 +0300

    feat: Add a search by login or email

--- src/repositories/users-mongo-repository.ts
+++ src/repositories/users-mongo-repository.ts
@@ -1,6 +1,7 @@
 import {ObjectId} from "mongodb";
 import {userCollection} from "../db/mongo-db";
 import {UserDbType} from "../db/user-db-type";
+import {LoginInputType} from "../types/auth-types";
 
 export const usersMongoRepository = {
     async create(inputUser: UserDbType): Promise<{ id: string }> {
@@ -11,6 +12,16 @@ export const usersMongoRepository = {
     async deleteById(findUser: UserDbType): Promise<boolean | null> {
         await userCollection.deleteOne(findUser)
         return true
+    },
+
+    async findByLoginOrEmail(inputAuth: LoginInputType): Promise<UserDbType | null> {
+        const filter = {
+            $or: [
+                {login: inputAuth.loginOrEmail},
+                {email: inputAuth.loginOrEmail},
+            ]
+        }
+        return await userCollection.findOne(filter)
     },
 
     async findById(id: ObjectId): Promise<UserDbType | null> {
