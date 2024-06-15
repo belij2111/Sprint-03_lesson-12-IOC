c0571be83c34a762b28457572dab926485a94914
belij2111 <belijvalerij2111@gmail.com>
2024-06-15 18:42:20 +0300

    feat: Add password generation creating a user

--- src/services/users-service.ts
+++ src/services/users-service.ts
@@ -3,11 +3,14 @@ import {dateTimeIsoString} from "../helpers/date-time -iso-string";
 import {InputUserType} from "../types/user-types";
 import {UserDbType} from "../db/user-db-type";
 import {usersMongoRepository} from "../repositories/users-mongo-repository";
+import {bcryptService} from "../adapters/bcrypt-service";
 
 export const usersService = {
     async createUser(inputUser: InputUserType): Promise<{ id: string }> {
+        const passHash = await bcryptService.generateHash(inputUser.password)
         const createNewUser: UserDbType = {
             ...inputUser,
+            password: passHash,
             _id: new ObjectId(),
             createdAt: dateTimeIsoString()
         }
