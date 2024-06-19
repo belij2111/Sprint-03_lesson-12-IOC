75fa8650bd70de5ba132b275671589587ccc1082
belij2111 <belijvalerij2111@gmail.com>
2024-06-20 00:07:22 +0300

    fix: Change the type to string

--- src/common/types/result-type.ts
+++ src/common/types/result-type.ts
@@ -2,6 +2,6 @@ import {ResultStatus} from "./result-code";
 
 export type Result<T = null> = {
     status: ResultStatus,
-    extensions?: [{ field: '', message: '' }],
+    extensions?: [{ field: string, message: string }],
     data: T
 }
