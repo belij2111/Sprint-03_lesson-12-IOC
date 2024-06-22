0b859d4605ef95ecc32fec19a90ff1a815d08cb5
belij2111 <belijvalerij2111@gmail.com>
2024-06-22 19:28:18 +0300

    refactor: Perform error handling using object Result

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -1,38 +1,27 @@
 import {Request, Response} from "express";
 import {authService} from "../services/auth-service";
+import {ResultStatus} from "../common/types/result-code";
 
 export const authController = {
     async login(req: Request, res: Response) {
         try {
             const result = await authService.loginUser(req.body)
-            if (!result) {
+            if (result.status === ResultStatus.Unauthorized) {
                 res
                     .status(401)
-                    .json({
-                        errorsMessages: [
-                            {
-                                message: 'Password or login is wrong',
-                                field: 'loginOrEmail'
-                            },
-                        ]
-                    })
+                    .json({errorsMessages: result.extensions || []})
                 return
             }
-            res
-                .status(204)
-                .json({})
+            if (result.status === ResultStatus.Success) {
+                res
+                    .status(204)
+                    .json({})
+                return
+            }
         } catch (error) {
             res
-                .status(400)
-                .json({
-                    errorsMessages: [
-                        {
-                            message: 'InputModel has incorrect values',
-                            field: "loginOrEmail"
-                        },
-                    ]
-                })
+                .status(500)
+                .json({message: 'authController.login'})
         }
-
     }
 }
--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -2,11 +2,36 @@ import {usersMongoRepository} from "../repositories/users-mongo-repository";
 import {LoginInputType} from "../types/auth-types";
 import {bcryptService} from "../common/adapters/bcrypt-service";
 import {UserDbType} from "../db/user-db-type";
+import {Result} from "../common/types/result-type";
+import {ResultStatus} from "../common/types/result-code";
 
 export const authService = {
-    async loginUser(inputAuth: LoginInputType): Promise<boolean> {
+    async loginUser(inputAuth: LoginInputType): Promise<Result<boolean | null>> {
         const userAuth: UserDbType | null = await usersMongoRepository.findByLoginOrEmail(inputAuth)
-        if (!userAuth) return false
-        return await bcryptService.checkPassword(inputAuth.password, userAuth.password)
+        if (!userAuth) {
+            return {
+                status: ResultStatus.Unauthorized,
+                extensions: [{field: 'login or email', message: 'Login or email is not unique'}],
+                data: null
+            }
+        }
+        const result = await bcryptService.checkPassword(inputAuth.password, userAuth.password)
+        if (!result) {
+            return {
+                status: ResultStatus.Unauthorized,
+                extensions: [{field: 'password', message: 'Password is wrong'}],
+                data: null
+            }
+        }
+        return {
+            status: ResultStatus.Success,
+            data: result
+        }
     }
+
+    // async loginUser(inputAuth: LoginInputType): Promise<boolean> {
+    //     const userAuth: UserDbType | null = await usersMongoRepository.findByLoginOrEmail(inputAuth)
+    //     if (!userAuth) return false
+    //     return await bcryptService.checkPassword(inputAuth.password, userAuth.password)
+    // }
 }
