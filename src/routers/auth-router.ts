7ae999a5a9e627363c7016c253ee08a0c487479a
belij2111 <belijvalerij2111@gmail.com>
2024-06-29 21:57:28 +0300

    feat: Add a route to get authenticated users

--- src/routers/auth-router.ts
+++ src/routers/auth-router.ts
@@ -1,6 +1,8 @@
 import {Router} from "express"
 import {authController} from "../controllers/auth-controller";
+import {authBearerMiddleware} from "../common/middlewares/auth-bearer-middleware";
 
 export const authRouter = Router()
 
 authRouter.post('/login', authController.login)
+authRouter.get('/me', authBearerMiddleware, authController.get)
