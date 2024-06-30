c45cf73d403c0c3102af9f7efb8b57a945c9ec6e
belij2111 <belijvalerij2111@gmail.com>
2024-06-30 23:58:45 +0300

    feat: Add a route to create comments

--- src/routers/posts-router.ts
+++ src/routers/posts-router.ts
@@ -1,5 +1,6 @@
 import {Router} from "express";
 import {
+    createCommentByPostIdController,
     createPostController, deletePostByIdController,
     getPostByIdController,
     getPostController,
@@ -8,6 +9,7 @@ import {
 import {authBasicMiddleware} from "../common/middlewares/auth-basic-middleware";
 import {inputValidationMiddleware} from "../common/middlewares/input-validation-middlware";
 import {postsInputValidationMiddleware} from "../validators/posts-input-validation-middleware";
+import {authBearerMiddleware} from "../common/middlewares/auth-bearer-middleware";
 
 export const postsRouter = Router()
 
@@ -16,3 +18,4 @@ postsRouter.get('/', getPostController)
 postsRouter.get('/:id', getPostByIdController)
 postsRouter.put('/:id', authBasicMiddleware, postsInputValidationMiddleware, inputValidationMiddleware, updatePostController)
 postsRouter.delete('/:id', authBasicMiddleware, inputValidationMiddleware, deletePostByIdController)
+postsRouter.post('/:postId/comments', authBearerMiddleware, inputValidationMiddleware, createCommentByPostIdController)
