fa1d299243e35f9cc48e5a552033dd5e04ffc654
belij2111 <belijvalerij2111@gmail.com>
2024-07-01 23:35:56 +0300

    feat: Add validation of input data for comments

--- src/routers/posts-router.ts
+++ src/routers/posts-router.ts
@@ -10,6 +10,7 @@ import {authBasicMiddleware} from "../common/middlewares/auth-basic-middleware";
 import {inputValidationMiddleware} from "../common/middlewares/input-validation-middlware";
 import {postsInputValidationMiddleware} from "../validators/posts-input-validation-middleware";
 import {authBearerMiddleware} from "../common/middlewares/auth-bearer-middleware";
+import {commentsInputValidationMiddleware} from "../validators/comments-input-validation-middleware";
 
 export const postsRouter = Router()
 
@@ -18,4 +19,4 @@ postsRouter.get('/', getPostController)
 postsRouter.get('/:id', getPostByIdController)
 postsRouter.put('/:id', authBasicMiddleware, postsInputValidationMiddleware, inputValidationMiddleware, updatePostController)
 postsRouter.delete('/:id', authBasicMiddleware, inputValidationMiddleware, deletePostByIdController)
-postsRouter.post('/:postId/comments', authBearerMiddleware, inputValidationMiddleware, createCommentByPostIdController)
+postsRouter.post('/:postId/comments', authBearerMiddleware, commentsInputValidationMiddleware, inputValidationMiddleware, createCommentByPostIdController)
