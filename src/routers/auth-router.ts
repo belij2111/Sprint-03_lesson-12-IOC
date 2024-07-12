ef61753ed5fde92c3517b13b311121e3962d1a25
belij2111 <belijvalerij2111@gmail.com>
2024-07-12 17:14:57 +0300

    feat: Add a route for refresh Tokens

--- src/routers/auth-router.ts
+++ src/routers/auth-router.ts
@@ -3,6 +3,7 @@ import {authController} from "../controllers/auth-controller";
 import {authBearerMiddleware} from "../common/middlewares/auth-bearer-middleware";
 import {usersInputValidationMiddleware} from "../validators/users-input-validation-middleware";
 import {inputValidationMiddleware} from "../common/middlewares/input-validation-middlware";
+import {refreshTokenMiddleware} from "../common/middlewares/refresh-token-middleware";
 
 export const authRouter = Router()
 
@@ -11,3 +12,4 @@ authRouter.get('/me', authBearerMiddleware, authController.get)
 authRouter.post('/registration', usersInputValidationMiddleware, inputValidationMiddleware, authController.registration)
 authRouter.post('/registration-confirmation', authController.registrationConfirmation)
 authRouter.post('/registration-email-resending', authController.registrationEmailResending)
+authRouter.post('/refresh-token', refreshTokenMiddleware, authController.refreshToken)
