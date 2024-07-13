0f91d0a1ba6f75d534fdc2b9be783bb046e91e25
belij2111 <belijvalerij2111@gmail.com>
2024-07-13 16:19:37 +0300

    feat: Add a check for the presence of a token in the blacklist

--- src/common/middlewares/refresh-token-middleware.ts
+++ src/common/middlewares/refresh-token-middleware.ts
@@ -1,6 +1,7 @@
 import {NextFunction, Request, Response} from "express";
 import {jwtService} from "../adapters/jwt-service";
 import {JwtPayload} from "jsonwebtoken";
+import {authMongoRepository} from "../../repositories/auth-mongo-repository";
 
 export const refreshTokenMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     const refreshToken = req.cookies.refreshToken
@@ -17,5 +18,13 @@ export const refreshTokenMiddleware = async (req: Request, res: Response, next: 
             .json({})
         return
     }
+    const tokenInBlacklist = await authMongoRepository.findInBlackList(refreshToken)
+    if (tokenInBlacklist) {
+        res
+            .status(401)
+            .json({})
+        return
+    }
+    req.user = {id: payload.userId}
     next()
 }
