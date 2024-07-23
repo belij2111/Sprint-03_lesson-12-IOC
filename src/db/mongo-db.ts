7565e688781efee8db6214f9a44aea3de96901b2
belij2111 <belijvalerij2111@gmail.com>
2024-07-23 23:47:26 +0300

    feat: Add collection for device sessions

--- src/db/mongo-db.ts
+++ src/db/mongo-db.ts
@@ -6,6 +6,7 @@ import {UserDbType} from "./user-db-type";
 import {CommentDbType} from "./comment-db-type";
 import {RefreshTokenDbType} from "./refresh-token-db-type";
 import {ApiCallDbType} from "./api-call-db-type";
+import {DeviceSessionsDbType} from "./device-sessions-db-type";
 
 let client = {} as MongoClient
 export let db = {} as Db
@@ -16,6 +17,7 @@ export let userCollection: Collection<UserDbType> = {} as Collection<UserDbType>
 export let commentCollection: Collection<CommentDbType> = {} as Collection<CommentDbType>
 export let refreshTokenCollection: Collection<RefreshTokenDbType> = {} as Collection<RefreshTokenDbType>
 export let apiCallsCollection: Collection<ApiCallDbType> = {} as Collection<ApiCallDbType>
+export let deviceSessionsCollection: Collection<DeviceSessionsDbType> = {} as Collection<DeviceSessionsDbType>
 
 export const connectToDb = async (DB_URL: string) => {
     try {
@@ -27,6 +29,7 @@ export const connectToDb = async (DB_URL: string) => {
         commentCollection = db.collection<CommentDbType>(SETTINGS.COMMENT_COLLECTION_NAME)
         refreshTokenCollection = db.collection<RefreshTokenDbType>(SETTINGS.REFRESH_TOKEN_COLLECTION_NAME)
         apiCallsCollection = db.collection<ApiCallDbType>(SETTINGS.API_CALLS_COLLECTION_NAME)
+        deviceSessionsCollection = db.collection<DeviceSessionsDbType>(SETTINGS.DEVICE_SESSIONS_COLLECTION_NAME)
         await client.connect()
         console.log('Connected to db')
         return true
