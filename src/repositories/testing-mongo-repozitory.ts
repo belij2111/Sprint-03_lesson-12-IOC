992b2c232d437ab4ca3ba45fe0e0ae7bd7530fa9
belij2111 <belijvalerij2111@gmail.com>
2024-07-27 19:55:27 +0300

    feat: Add collections cleanup

--- src/repositories/testing-mongo-repozitory.ts
+++ src/repositories/testing-mongo-repozitory.ts
@@ -1,4 +1,11 @@
-import {blogCollection, commentCollection, postCollection, userCollection} from "../db/mongo-db";
+import {
+    apiCallsCollection,
+    blogCollection,
+    commentCollection, deviceSessionsCollection,
+    postCollection,
+    refreshTokenCollection,
+    userCollection
+} from "../db/mongo-db";
 
 export const testingMongoRepository = {
     async deleteAllData() {
@@ -6,5 +13,8 @@ export const testingMongoRepository = {
         await postCollection.deleteMany()
         await userCollection.deleteMany()
         await commentCollection.deleteMany()
+        await refreshTokenCollection.deleteMany()
+        await apiCallsCollection.deleteMany()
+        await deviceSessionsCollection.deleteMany()
     }
 }
