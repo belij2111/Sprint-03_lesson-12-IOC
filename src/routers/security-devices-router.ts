f4ccede608d3a91dee4fb38e6eb028ababb58549
belij2111 <belijvalerij2111@gmail.com>
2024-07-27 22:27:27 +0300

    feat: Add a route to return all devices with active sessions for the current user

---
+++ src/routers/security-devices-router.ts
@@ -0,0 +1,7 @@
+import {Router} from "express"
+import {securityDevicesController} from "../controllers/security-devices-controller";
+import {refreshTokenMiddleware} from "../common/middlewares/refresh-token-middleware";
+
+export const securityDevicesRouter = Router()
+
+securityDevicesRouter.get('/devices', refreshTokenMiddleware, securityDevicesController.get)
