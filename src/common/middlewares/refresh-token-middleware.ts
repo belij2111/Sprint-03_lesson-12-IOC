ef96d3340925a42012c5d705efb24626a9d4fa98
belij2111 <belijvalerij2111@gmail.com>
2024-07-28 16:58:13 +0300

    feat: Add session and token version verification

--- src/common/middlewares/refresh-token-middleware.ts
+++ src/common/middlewares/refresh-token-middleware.ts
@@ -1,6 +1,7 @@
 import {NextFunction, Request, Response} from "express";
 import {jwtService} from "../adapters/jwt-service";
 import {JwtPayload} from "jsonwebtoken";
+import {securityDevicesMongoRepository} from "../../repositories/security-devices-mongo-repository";
 
 export const refreshTokenMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     const refreshToken = req.cookies.refreshToken
@@ -12,6 +13,21 @@ export const refreshTokenMiddleware = async (req: Request, res: Response, next: 
     }
     const payload = await jwtService.verifyToken(refreshToken) as JwtPayload
     if (!payload) {
+        res
+            .status(401)
+            .json({})
+        return
+    }
+    const deviceSession = await securityDevicesMongoRepository.findByDeviceId(payload.deviceId)
+    if (!deviceSession) {
+        res
+            .status(401)
+            .json({})
+        return
+    }
+    const tokenVersionFromPayload = new Date(payload.iat! * 1000).toISOString()
+    const tokenVersionInDB = deviceSession.iatDate
+    if (tokenVersionFromPayload !== tokenVersionInDB) {
         res
             .status(401)
             .json({})
