eec2c83a0cbcb15afedf4cab25e1a8b6858283a3
belij2111 <belijvalerij2111@gmail.com>
2024-07-28 11:08:48 +0300

    feat: Add the deviceId field to the request

--- src/common/types/index.d.ts
+++ src/common/types/index.d.ts
@@ -4,7 +4,8 @@ import {JwtPayload} from "jsonwebtoken";
 declare global {
     namespace Express {
         export interface Request {
-            user: JwtPayload
+            user: JwtPayload,
+            deviceId: string
         }
     }
 }
