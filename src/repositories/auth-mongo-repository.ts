ae3f6b3fd987dfe1f5e0ddbfdb6aafcdbaed7231
belij2111 <belijvalerij2111@gmail.com>
2024-07-28 18:40:57 +0300

    refactor: Delete unused blacklist implementation code for token

--- src/repositories/auth-mongo-repository.ts
+++ src/repositories/auth-mongo-repository.ts
@@ -1,22 +1,8 @@
-import {apiCallsCollection, refreshTokenCollection} from "../db/mongo-db";
-import {RefreshTokenDbType} from "../db/refresh-token-db-type";
-import {ObjectId} from "mongodb";
+import {apiCallsCollection} from "../db/mongo-db";
 import {ApiCallDataInputType} from "../types/auth-types";
 import {ApiCallDbType} from "../db/api-call-db-type";
 
 export const authMongoRepository = {
-    async findInBlackList(refreshToken: string) {
-        return await refreshTokenCollection.findOne({refreshToken})
-    },
-
-    async addToBlackList(refreshToken: string) {
-        const tokenWithId: RefreshTokenDbType = {
-            _id: new ObjectId(),
-            refreshToken
-        }
-        return await refreshTokenCollection.insertOne(tokenWithId)
-    },
-
     async addApiCall(apiCall: ApiCallDbType) {
         await apiCallsCollection.insertOne(apiCall)
     },
