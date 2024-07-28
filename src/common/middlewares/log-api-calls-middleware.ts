d7ec95d71c24a72e6091a72e8f3fae1442dacb96
belij2111 <belijvalerij2111@gmail.com>
2024-07-28 18:04:02 +0300

    fix: Change the check for the presence of ip and origin url in the request

--- src/common/middlewares/log-api-calls-middleware.ts
+++ src/common/middlewares/log-api-calls-middleware.ts
@@ -4,8 +4,14 @@ import {ApiCallDataInputType} from "../../types/auth-types";
 import {ResultStatus} from "../types/result-code";
 
 export const logApiCallsMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    if (!req.ip) {
+        return next(new Error("IP address is undefined"));
+    }
+    if (!req.originalUrl) {
+        return next(new Error("Original url is undefined"));
+    }
     const apiCallData: ApiCallDataInputType = {
-        ip: req.ip || '', //todo: req.ip can return undefined
+        ip: req.ip,
         url: req.originalUrl
     }
     const result = await authService.checkApiCalls(apiCallData)
