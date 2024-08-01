aea6d3c2506cef0c3fdae1ca844b6544d182975f
belij2111 <belijvalerij2111@gmail.com>
2024-08-01 23:57:14 +0300

    refactor: Use the object result pattern and wrap all methods in a try-catch

--- src/controllers/blogs-controller.ts
+++ src/controllers/blogs-controller.ts
@@ -12,95 +12,141 @@ import {
 } from "../common/helpers/sort-query-fields-util";
 import {blogsService} from "../services/blogs-service";
 import {postsService} from "../services/posts-service";
+import {ResultStatus} from "../common/types/result-code";
+import {ErrorResponse} from "../common/types/error-response";
 
 export const blogsController = {
     async create(req: Request, res: Response) {
-        const createdInfo = await blogsService.createBlog(req.body)
-        const newBlog = await blogsMongoQueryRepository.getBlogById(createdInfo.id)
-        res
-            .status(201)
-            .json(newBlog)
+        try {
+            const createdInfo = await blogsService.createBlog(req.body)
+            if (createdInfo.data && createdInfo.status === ResultStatus.Success) {
+                const newBlog = await blogsMongoQueryRepository.getBlogById(createdInfo.data.id)
+                res
+                    .status(201)
+                    .json(newBlog)
+                return
+            }
+        } catch (error) {
+            res
+                .status(500)
+                .json({message: 'blogsController.create'})
+        }
     },
 
     async createPostByBlogId(req: Request, res: Response) {
-        const createdInfo = await postsService.createPostByBlogId(req.params.blogId, req.body)
-        if (!createdInfo) {
+        try {
+            const createdInfo = await postsService.createPostByBlogId(req.params.blogId, req.body)
+            if (!createdInfo) {
+                res
+                    .status(404)
+                    .json({message: 'Blog not found'})
+                return
+            }
+            const newPost = await postsMongoQueryRepository.getPostById(createdInfo.id)
             res
-                .status(404)
-                .json({message: 'Blog not found'})
-            return
+                .status(201)
+                .json(newPost)
+        } catch (error) {
+            res
+                .status(500)
+                .json({message: 'blogsController.createPostByBlogId'})
         }
-        const newPost = await postsMongoQueryRepository.getPostById(createdInfo.id)
-        res
-            .status(201)
-            .json(newPost)
     },
 
-    async get(req: Request<SortQueryFieldsType & SearchNameTermFieldsType>, res: Response<Paginator<OutputBlogType[]>>) {
-        const inputQuery = {
-            ...sortQueryFieldsUtil(req.query),
-            ...searchNameTermUtil(req.query)
+    async get(req: Request<SortQueryFieldsType & SearchNameTermFieldsType>, res: Response<Paginator<OutputBlogType[]> | ErrorResponse>) {
+        try {
+            const inputQuery = {
+                ...sortQueryFieldsUtil(req.query),
+                ...searchNameTermUtil(req.query)
+            }
+            const allBlogs = await blogsMongoQueryRepository.getBlogs(inputQuery)
+            res
+                .status(200)
+                .json(allBlogs)
+        } catch (error) {
+            res
+                .status(500)
+                .json({message: 'blogsController.get'})
         }
-        const allBlogs = await blogsMongoQueryRepository.getBlogs(inputQuery)
-        res
-            .status(200)
-            .json(allBlogs)
     },
 
-    async getById(req: Request, res: Response<OutputBlogType>) {
-        const blogId = req.params.id
-        const blog = await blogsMongoQueryRepository.getBlogById(blogId)
-        if (!blog) {
+    async getById(req: Request, res: Response<OutputBlogType | ErrorResponse>) {
+        try {
+            const blogId = req.params.id
+            const blog = await blogsMongoQueryRepository.getBlogById(blogId)
+            if (!blog) {
+                res
+                    .sendStatus(404)
+                return
+            }
             res
-                .sendStatus(404)
-            return
+                .status(200)
+                .json(blog)
+        } catch (error) {
+            res
+                .status(500)
+                .json({message: 'blogsController.getById'})
         }
-        res
-            .status(200)
-            .json(blog)
     },
 
-    async getPostsByBlogId(req: Request, res: Response<Paginator<OutputPostType[]>>) {
-        const postBlogId = req.params.blogId
-        const inputQuery = {
-            ...sortQueryFieldsUtil(req.query)
-        }
-        const posts = await postsMongoQueryRepository.getPostsByBlogId(postBlogId, inputQuery)
-        if (!posts) {
+    async getPostsByBlogId(req: Request, res: Response<Paginator<OutputPostType[]> | ErrorResponse>) {
+        try {
+            const postBlogId = req.params.blogId
+            const inputQuery = {
+                ...sortQueryFieldsUtil(req.query)
+            }
+            const posts = await postsMongoQueryRepository.getPostsByBlogId(postBlogId, inputQuery)
+            if (!posts) {
+                res
+                    .sendStatus(404)
+                return
+            }
             res
-                .sendStatus(404)
-            return
+                .status(200)
+                .json(posts)
+        } catch (error) {
+            res
+                .status(500)
+                .json({message: 'blogsController.getPostsByBlogId'})
         }
-        res
-            .status(200)
-            .json(posts)
     },
 
     async updateById(req: Request<{ id: string }, {}, InputBlogType>, res: Response) {
-        const updateBlog = await blogsService.updateBlogById(req.params.id, req.body)
-        if (!updateBlog) {
+        try {
+            const updateBlog = await blogsService.updateBlogById(req.params.id, req.body)
+            if (updateBlog.status === ResultStatus.NotFound) {
+                res
+                    .status(404)
+                    .json({errorsMessages: updateBlog.extensions || []})
+                return
+            }
             res
-                .status(404)
-                .json({message: 'Blog not found'})
-            return
+                .status(204)
+                .json({errorsMessages: updateBlog.extensions || []})
+        } catch (error) {
+            res
+                .status(500)
+                .json({message: 'blogsController.updateById'})
         }
-        res
-            .status(204)
-            .json({message: "successfully updated"})
     },
 
     async deleteById(req: Request<{ id: string }>, res: Response) {
-        const deleteBlog = await blogsService.deleteBlogById(req.params.id)
-        if (!deleteBlog) {
+        try {
+            const deleteBlog = await blogsService.deleteBlogById(req.params.id)
+            if (deleteBlog.status === ResultStatus.NotFound) {
+                res
+                    .status(404)
+                    .json({errorsMessages: deleteBlog.extensions || []})
+                return
+            }
             res
-                .status(404)
-                .json({message: 'Blog not found'})
-
-            return
+                .status(204)
+                .json({errorsMessages: deleteBlog.extensions || []})
+        } catch (error) {
+            res
+                .status(500)
+                .json({message: 'blogsController.deleteById'})
         }
-        res
-            .status(204)
-            .json({message: 'Blog deleted successfully'})
     }
 }
 
--- src/services/blogs-service.ts
+++ src/services/blogs-service.ts
@@ -3,32 +3,56 @@ import {BlogDBType} from "../db/blog-db-type";
 import {ObjectId} from "mongodb";
 import {dateTimeIsoString} from "../common/helpers/date-time-iso-string";
 import {blogsMongoRepository} from "../repositories/blogs-mongo-repository";
+import {Result} from "../common/types/result-type";
+import {ResultStatus} from "../common/types/result-code";
 
 export const blogsService = {
-    async createBlog(inputBlog: InputBlogType): Promise<{ id: string }> {
+    async createBlog(inputBlog: InputBlogType): Promise<Result<{ id: string }>> {
         const createNewBlog: BlogDBType = {
             ...inputBlog,
             _id: new ObjectId(),
             createdAt: dateTimeIsoString(),
             isMembership: false
         }
-        return await blogsMongoRepository.create(createNewBlog)
+        const result = await blogsMongoRepository.create(createNewBlog)
+        return {
+            status: ResultStatus.Success,
+            data: result
+        }
     },
 
-    async updateBlogById(id: string, inputBlog: InputBlogType): Promise<boolean | null> {
+    async updateBlogById(id: string, inputBlog: InputBlogType): Promise<Result<boolean | null>> {
         const findBlog = await blogsMongoRepository.findById(new ObjectId(id))
-        if (!findBlog) return null
+        if (!findBlog)
+            return {
+                status: ResultStatus.NotFound,
+                extensions: [{field: 'findBlog', message: 'Blog not found'}],
+                data: null
+            }
         const updateBlog = {
             name: inputBlog.name,
             description: inputBlog.description,
             websiteUrl: inputBlog.websiteUrl
         }
-        return await blogsMongoRepository.updateById(findBlog, updateBlog)
+        const result = await blogsMongoRepository.updateById(findBlog, updateBlog)
+        return {
+            status: ResultStatus.Success,
+            data: result
+        }
     },
 
-    async deleteBlogById(id: string): Promise<boolean | null> {
+    async deleteBlogById(id: string): Promise<Result<boolean | null>> {
         const findBlog = await blogsMongoRepository.findById(new ObjectId(id))
-        if (!findBlog) return null
-        return await blogsMongoRepository.deleteById(findBlog)
+        if (!findBlog)
+            return {
+                status: ResultStatus.NotFound,
+                extensions: [{field: 'findBlog', message: 'Blog not found'}],
+                data: null
+            }
+        const result = await blogsMongoRepository.deleteById(findBlog)
+        return {
+            status: ResultStatus.Success,
+            data: result
+        }
     },
 }
