12f82934a1ed629909f05fe96ba90c395e80d905
belij2111 <belijvalerij2111@gmail.com>
2024-08-05 23:26:46 +0300

    refactor: Update the database connection using mongoose

--- src/db/mongo-db.ts
+++ src/db/mongo-db.ts
@@ -1,44 +1,35 @@
-import {Db, MongoClient} from "mongodb"
-import {SETTINGS} from "../settings"
-import {BlogDBType} from "./blog-db-type";
-import {PostDbType} from "./post-db-type";
-import {UserDbType} from "./user-db-type";
-import {CommentDbType} from "./comment-db-type";
-import {RefreshTokenDbType} from "./refresh-token-db-type";
-import {ApiCallDbType} from "./api-call-db-type";
-import {DeviceSessionsDbType} from "./device-sessions-db-type";
+import mongoose from 'mongoose'
+import {BlogModel} from "../domain/blog.entity";
+import {PostModel, PostSchema} from "../domain/post.entity";
+import {CommentModel, CommentSchema} from "../domain/comment.entity";
+import {UserModel} from "../domain/user.entity";
+import {RefreshTokenModel} from "../domain/refresh-token.entity";
+import {ApiCallsModel} from "../domain/api-calls.entity";
+import {DeviceSessionsModel} from "../domain/device-sessions.entity";
 
 export const db = {
-    client: {} as MongoClient,
-    getDbName(): Db {
-        return this.client.db(SETTINGS.DB_NAME)
-    },
-
     async run(DB_URL: string): Promise<boolean> {
         try {
-            this.client = new MongoClient(DB_URL)
-            await this.client.connect()
-            await this.getDbName().command({ping: 1})
+            await mongoose.connect(DB_URL)
             console.log('Connected to db')
             return true
         } catch (e) {
             console.log(e)
-            await this.client.close()
+            await mongoose.disconnect()
             return false
         }
     },
 
     async stop() {
-        await this.client.close()
+        await mongoose.disconnect()
         console.log('Connected  successfully closed')
     },
 
     async drop() {
         try {
-            const collections = await this.getDbName().listCollections().toArray()
-            for (const collection of collections) {
-                const collectionName = collection.name
-                await this.getDbName().collection(collectionName).deleteMany({})
+            const collections = mongoose.connection.collections
+            for (const key in collections) {
+                await collections[key].deleteMany({})
             }
         } catch (e) {
             console.error('Error in drop db', e)
@@ -48,13 +39,13 @@ export const db = {
 
     getCollections() {
         return {
-            blogCollection: this.getDbName().collection<BlogDBType>(SETTINGS.BLOG_COLLECTION_NAME as string),
-            postCollection: this.getDbName().collection<PostDbType>(SETTINGS.POST_COLLECTION_NAME as string),
-            userCollection: this.getDbName().collection<UserDbType>(SETTINGS.USER_COLLECTION_NAME as string),
-            commentCollection: this.getDbName().collection<CommentDbType>(SETTINGS.COMMENT_COLLECTION_NAME as string),
-            refreshTokenCollection: this.getDbName().collection<RefreshTokenDbType>(SETTINGS.REFRESH_TOKEN_COLLECTION_NAME as string),
-            apiCallsCollection: this.getDbName().collection<ApiCallDbType>(SETTINGS.API_CALLS_COLLECTION_NAME as string),
-            deviceSessionsCollection: this.getDbName().collection<DeviceSessionsDbType>(SETTINGS.DEVICE_SESSIONS_COLLECTION_NAME as string)
+            blogCollection: BlogModel,
+            postCollection: PostModel,
+            userCollection: UserModel,
+            commentCollection: CommentModel,
+            refreshTokenCollection: RefreshTokenModel,
+            apiCallsCollection: ApiCallsModel,
+            deviceSessionsCollection: DeviceSessionsModel
         }
     }
 }
