4733eeecfe4c4156be5516cfaf16eb9788db345f
belij2111 <belijvalerij2111@gmail.com>
2024-08-12 23:01:30 +0300

    feat: Update a types for comments

--- src/db/comment-db-type.ts
+++ src/db/comment-db-type.ts
@@ -1,5 +1,11 @@
 import {ObjectId} from "mongodb";
 
+export enum LikeStatus {
+    None = "None",
+    Like = "Like",
+    Dislike = "Dislike"
+}
+
 export class CommentDbType {
     constructor(
         public _id: ObjectId,
@@ -9,7 +15,12 @@ export class CommentDbType {
             userLogin: string
         },
         public createdAt: string,
-        public postId: ObjectId
+        public postId: ObjectId,
+        public likesInfo: {
+            likesCount: number,
+            dislikesCount: number,
+            myStatus: LikeStatus
+        }
     ) {
     }
 }
