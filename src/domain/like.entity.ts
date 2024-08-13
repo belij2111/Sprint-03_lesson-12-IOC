6b49075b0fe4431331b9501598be37d19a9c1723
belij2111 <belijvalerij2111@gmail.com>
2024-08-13 23:12:25 +0300

    feat: Add a model for likes

---
+++ src/domain/like.entity.ts
@@ -0,0 +1,13 @@
+import mongoose, {Schema} from "mongoose";
+import {SETTINGS} from "../settings";
+import {LikeDbType, LikeStatus} from "../db/like-db-type";
+
+const LikeSchema = new mongoose.Schema<LikeDbType>({
+    _id: {type: Schema.Types.ObjectId, required: true},
+    createdAt: {type: String, required: true},
+    status: {type: String, enum: LikeStatus, default: LikeStatus.None},
+    authorId: {type: String, required: true},
+    parentId: {type: String, required: true}
+})
+
+export const LikeModel = mongoose.model<LikeDbType>(SETTINGS.COLLECTION_NAME.LIKES, LikeSchema)
