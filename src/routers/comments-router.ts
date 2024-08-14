7958a0a91e3222fe59108a0c65e5c571907bfccb
belij2111 <belijvalerij2111@gmail.com>
2024-08-14 23:00:57 +0300

    feat: Add a route for likes

--- src/routers/comments-router.ts
+++ src/routers/comments-router.ts
@@ -3,9 +3,11 @@ import {commentsController} from "../controllers/comments-controller";
 import {authBearerMiddleware} from "../common/middlewares/auth-bearer-middleware";
 import {commentsInputValidationMiddleware} from "../validators/comments-input-validation-middleware";
 import {inputValidationMiddleware} from "../common/middlewares/input-validation-middlware";
+import {likeStatusInputValidationMiddleware} from "../validators/like-status-input-validation-middleware";
 
 export const commentsRouter = Router()
 
 commentsRouter.get('/:id', commentsController.getById.bind(commentsController))
 commentsRouter.put('/:commentId', authBearerMiddleware.checkAuth.bind(authBearerMiddleware), commentsInputValidationMiddleware, inputValidationMiddleware, commentsController.update.bind(commentsController))
 commentsRouter.delete('/:commentId', authBearerMiddleware.checkAuth.bind(authBearerMiddleware), inputValidationMiddleware, commentsController.delete.bind(commentsController))
+commentsRouter.put('/:commentId/like-status', authBearerMiddleware.checkAuth.bind(authBearerMiddleware), likeStatusInputValidationMiddleware, inputValidationMiddleware, commentsController.updateLikeStatus.bind(commentsController))
