1642b07828fa083f15e3fa16a15e8920d51ede6e
belij2111 <belijvalerij2111@gmail.com>
2024-09-08 23:46:19 +0300

    feat: Add a router for update like-status

--- src/routers/posts-router.ts
+++ src/routers/posts-router.ts
@@ -6,13 +6,15 @@ import {postsInputValidationMiddleware} from "../validators/posts-input-validati
 import {authBearerMiddleware} from "../common/middlewares/auth-bearer-middleware";
 import {commentsInputValidationMiddleware} from "../validators/comments-input-validation-middleware";
 import {userIdentificationMiddleware} from "../common/middlewares/user-identification-middleware";
+import {likeStatusInputValidationMiddleware} from "../validators/like-status-input-validation-middleware";
 
 export const postsRouter = Router()
 
 postsRouter.post('/', authBasicMiddleware, postsInputValidationMiddleware, inputValidationMiddleware, postsController.create.bind(postsController))
-postsRouter.get('/', postsController.get.bind(postsController))
-postsRouter.get('/:id', postsController.getById.bind(postsController))
+postsRouter.get('/', userIdentificationMiddleware.identifyUser.bind(userIdentificationMiddleware), postsController.get.bind(postsController))
+postsRouter.get('/:id', userIdentificationMiddleware.identifyUser.bind(userIdentificationMiddleware), postsController.getById.bind(postsController))
 postsRouter.put('/:id', authBasicMiddleware, postsInputValidationMiddleware, inputValidationMiddleware, postsController.update.bind(postsController))
 postsRouter.delete('/:id', authBasicMiddleware, inputValidationMiddleware, postsController.deleteById.bind(postsController))
 postsRouter.post('/:postId/comments', authBearerMiddleware.checkAuth.bind(authBearerMiddleware), commentsInputValidationMiddleware, inputValidationMiddleware, postsController.createCommentByPostId.bind(postsController))
 postsRouter.get('/:postId/comments', userIdentificationMiddleware.identifyUser.bind(userIdentificationMiddleware), postsController.getCommentsByPostId.bind(postsController))
+postsRouter.put('/:postId/like-status', authBearerMiddleware.checkAuth.bind(authBearerMiddleware), likeStatusInputValidationMiddleware, inputValidationMiddleware, postsController.updateLikeStatus.bind(postsController))
